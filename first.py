02a04acfbf773c513766e930f15b4b12f4183a29
Aayush pokharel <pokharelaayush199@gmail.com>
2022-08-28 18:37:04 +0545

    function

---
+++ first.py
@@ -0,0 +1,52 @@
+'''
+syntax
+    defining function:
+    def function_name():
+    ...you can do anything here in this line...
+    return........... something
+'''
+#calling function:-
+    #function_name()
+
+
+
+
+
+
+
+
+#example
+'''def duita_sum_joda():
+    sum = 3+5
+    print(sum)
+    #return sum
+duita_sum_joda()
+'''
+
+#taking input form user
+'''first_name = input("enter your first name:-")
+second_name = input("enter your second name:-")
+full_name = first_name + second_name
+print("your full name is ",full_name)'''
+
+#string formating 
+'''name = input("enter your name:-")
+print(f"your name is {name}")'''
+
+f = -1
+s = -2
+t = -3
+f0 = -4
+fi = -5
+
+'''print(f"my{f} name{s} is{t} aayush{f0} pokharel{fi}")'''
+
+
+#len function
+'''name = "my name is aayush"
+print(len(name))
+def cale_weight(m,g=9.8):
+    print(f"your weight is {m*g}n")
+    
+mass = int(input("please enter your mass"))
+print(cale_weight(mass))'''
